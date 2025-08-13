694aac8d87b3e649961119aa7b2cdc35a0fad4c1
Thamerloai84 <Thamerloai84@gmail.com>
2025-08-13 21:38:47 +0300

    Create file.sh

---
+++ file.sh
@@ -0,0 +1,5 @@
+sudo pacman -Syu --needed --noconfirm \
+    base-devel git git-lfs ccache bc bison flex gperf \
+    imagemagick lib32-readline lib32-zlib libelf liblz4 \
+    SDL2 python python2 zlib lzop pngcrush rsync \
+    squashfs-tools xsltproc zip ncurses jdk11-openjdk
