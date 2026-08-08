c6e93f9c1950ceb5db3780d6deb729ffd62282d2
Thamerloai84 <Thamerloai84@gmail.com>
2026-08-08 22:04:16 +0300

    Fix syntax error in loading Orion Library

--- Secret Killer.lua
+++ Secret Killer.lua
@@ -1,5 +1,5 @@
 -- Orion Library UI for the requested features
-local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+local OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/jensonhirst/Orion/main/source'))()
 
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
